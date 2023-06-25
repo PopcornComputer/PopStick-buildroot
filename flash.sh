a2d4380987df00d381c1fa494c55d43611e38876
Jose Angel Torres <vonneh@gmail.com>
2023-06-25 20:03:48 +0800

    fix error in actions file; run make all;

--- flash.sh
+++ flash.sh
@@ -1,24 +1,28 @@
 #!/usr/bin/env zsh
-
-set -ex
+setopt aliases
+alias trace_on='set -ex'
+alias trace_off='{ PREV_STATUS=$? ; set +x; } 2>/dev/null; exit $PREV_STATUS'
+echo "Starting PopStick flashing sequence..."
+CURRENT_DIR=$(basename "$PWD")
 
 flash() {
+    trace_on
     xfel spinand erase 0x000000 134217728
     xfel spinand splwrite 0x400 0x0 sunxi-spl.bin
     xfel spinand write 0x20000 u-boot.img
     xfel spinand write 0x100000 rootfs.ubi
 
     xfel reset
+    trace_off
 }
 
-CURRENT_DIR=$(basename "$PWD")
-
-if [[ $CURRENT_DIR != images ]]
+if [[ $CURRENT_DIR = PopStick-buildroot ]]
 then
     pushd output/images
     flash
     popd
-elif [[ $CURRENT_DIR = images ]]
-then
+else
     flash
 fi
+
+echo "Done"
